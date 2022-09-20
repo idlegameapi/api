be03723c87e7b388d0a847a6f75d0f173218dc33
Micah Benac <micahbenac52@gmail.com>
2022-09-20 17:03:04 -0400

    magic

---
+++ README.md
@@ -0,0 +1,3 @@
+# IdleGameAPI
+An idle game handled entirely by an very epicc API
+*this is a very epicc README, Toga! smh*
---
+++ src/config.rs
@@ -0,0 +1,24 @@
+use dotenv::vars;
+
+#[derive(Debug)]
+pub struct Config {
+    pub db_url: String,
+}
+impl Config {
+    pub fn new() -> Self {
+        let environment_vars: Vec<(String, String)> = vars().collect();
+        Config {
+            db_url: find_key(&environment_vars, "DATABASE_URL"),
+        }
+    }
+}
+
+pub fn find_key(iteration: &[(String, String)], key_search: &'static str) -> String {
+    match iteration.iter().find(|(key, _)| key == key_search) {
+        Some((_, value)) => value.to_string(),
+        None => panic!(
+            "couldn't find '{}' in the environment variables",
+            key_search
+        ),
+    }
+}
